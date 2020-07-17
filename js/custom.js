ce9327a40ed78ea9323b16b01f00fa8dd5b8f4b7
Kushan Tharaka <tharakakushan@gmail.com>
2020-07-17 17:04:51 +0530

    Added My CV with a floating button, I am not sure about implemented functions in floating button,

--- js/custom.js
+++ js/custom.js
@@ -138,3 +138,26 @@
   new WOW().init();
 
 })(jQuery);
+
+
+//Get the button
+var mybutton = document.getElementById("myCvBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+
+}
